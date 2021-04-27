3f4ffc63cbb29c8747b74806122ac6c5d81da725
yougi <m.biagooi@gmail.com>
2021-04-27 18:46:48 +0430

    fixed cmake for boost python.

--- cmake/configure/configure_boost_python.cmake
+++ cmake/configure/configure_boost_python.cmake
@@ -26,16 +26,51 @@
 if (CAVIAR_WITH_BOOST_PYTHON)
   set (CAVIAR_WITH_BOOST_PYTHON ON)
 
-  find_package(BOOSTPYTHON)
   
-  find_package(PYTHON)
+  #find_library(Bpython libboost_python36.so PATHS /usr/local/lib/)  
+  
+  #INCLUDE_DIRECTORIES(/usr/include/python3.6)
+  
+
+  FIND_PACKAGE(PythonInterp 3 REQUIRED)
+
+  if (PYTHONINTERP_FOUND)
+    if (UNIX AND NOT APPLE)
+      if (PYTHON_VERSION_MAJOR EQUAL 3)
+          FIND_PACKAGE(Boost COMPONENTS python${PYTHON_VERSION_SUFFIX})
+          FIND_PACKAGE(PythonInterp 3)
+          FIND_PACKAGE(PythonLibs 3 REQUIRED)
+      else()
+          FIND_PACKAGE(Boost COMPONENTS python)
+          FIND_PACKAGE(PythonInterp)
+          FIND_PACKAGE(PythonLibs REQUIRED)
+      endif()
+    else()	
+      if (PYTHON_VERSION_MAJOR EQUAL 3)
+          FIND_PACKAGE(Boost COMPONENTS python${PYTHON_VERSION_MAJOR}${PYTHON_VERSION_MINOR})
+          FIND_PACKAGE(PythonInterp 3)
+          FIND_PACKAGE(PythonLibs 3 REQUIRED)
+      else()
+          FIND_PACKAGE(Boost COMPONENTS python${PYTHON_VERSION_MAJOR}${PYTHON_VERSION_MINOR})
+          FIND_PACKAGE(PythonInterp)
+          FIND_PACKAGE(PythonLibs REQUIRED)
+      endif()
+    endif()
+  else()
+      message("Python not found")
+  endif()
+
+  message(STATUS "PYTHON_LIBRARIES = ${PYTHON_LIBRARIES}")
+  message(STATUS "PYTHON_EXECUTABLE = ${PYTHON_EXECUTABLE}")
+  message(STATUS "PYTHON_INCLUDE_DIRS = ${PYTHON_INCLUDE_DIRS}")
+  message(STATUS "Boost_LIBRARIES = ${Boost_LIBRARIES}")
+  
+  INCLUDE_DIRECTORIES(${Boost_INCLUDE_DIRS} ${PYTHON_INCLUDE_DIRS})
+  #LINK_LIBRARIES(${Boost_LIBRARIES} ${PYTHON_LIBRARIES}) # Deprecated but so convenient!
 
   add_definitions (-DCAVIAR_WITH_BOOST_PYTHON)
 
-  #set (CMAKE_CXX_COMPILER ${MPI_CXX_COMPILER})
-  #set (CMAKE_C_COMPILER ${MPI_C_COMPILER}) 
-
-  #message (STATUS "Set CXX_COMPILER TO ${MPI_CXX_COMPILER}")  
+    
 else ()
   set (CAVIAR_WITH_BOOST_PYTHON OFF)
 endif ()
