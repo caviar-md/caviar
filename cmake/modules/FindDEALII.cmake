f960b78e4b826cc85b89df6ae3d69673ecf65154
moradbiagooi <m.biagooi@gmail.com>
2023-10-04 17:42:21 +0330

    Adding deal.II library information to CMake

--- cmake/modules/FindDEALII.cmake
+++ cmake/modules/FindDEALII.cmake
@@ -31,10 +31,29 @@ IF(NOT ${deal.II_FOUND})
 ELSE()
   add_compile_definitions(DEALII_VERSION_MAJOR=${deal.II_VERSION_MAJOR})
   add_compile_definitions(DEALII_VERSION_MINOR=${deal.II_VERSION_MINOR})
+  MESSAGE(STATUS "deal.II Version = ${deal.II_VERSION_MAJOR}.${deal.II_VERSION_MINOR}")
 ENDIF()
  
 DEAL_II_INITIALIZE_CACHED_VARIABLES()
 
+MESSAGE(STATUS "deal.II deal.II_FOUND =" ${deal.II_FOUND}) # Writes 1 or 0 (I guess)
+MESSAGE(STATUS "DEAL_II_DIR =" ${DEAL_II_DIR})  # writes nothig
+MESSAGE(STATUS "Environment(DEAL_II_DIR) =" $ENV{DEAL_II_DIR})  # writes nothig
+MESSAGE(STATUS "deal.II_DIR =" ${deal.II_DIR})  # writes something such as /usr/share/cmake/deal.II
+#MESSAGE(STATUS "deal.II_LIBRARIES =" ${deal.II_LIBRARIES})  # writes nothig
+#MESSAGE(STATUS "deal.II_INCLUDE_DIRS =" ${deal.II_INCLUDE_DIRS})  # writes nothig
+#MESSAGE(STATUS "deal.II_DIR =" ${deal.II_LIBRARIES})  # writes nothig
+
+
+#MESSAGE(STATUS "")
+#MESSAGE(STATUS "deal.II deal.II_FOUND =" ${deal.II_FOUND}) # Writes 1 or 0 (I guess)
+
+
+#MESSAGE(STATUS "")
+#MESSAGE(STATUS "DEAL_II_INCLUDE_DIRS:" ${DEAL_II_INCLUDE_DIRS}) # Writes so many directories without space
+
+#MESSAGE(STATUS "")
+#MESSAGE(STATUS "DEAL_II_LIBRARIES:" ${DEAL_II_LIBRARIES}) # Writes so many libraries without space
 # ===============================
 # =======                 =======
 # ===============================
