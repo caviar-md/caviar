73692a9fdb202079e72da1db1df33f7c536c0c83
moradbiagooi <m.biagooi@gmail.com>
2023-10-12 08:26:41 +0330

    Umbrella_sampling development...

---
+++ include/caviar/objects/force_field/umbrella_sampling.h
@@ -0,0 +1,90 @@
+
+//========================================================================
+//
+// Copyright (C) 2019 by Morad Biagooi and Ehsan Nedaaee Oskoee.
+//
+// This file is part of the CAVIAR package.
+//
+// The CAVIAR package is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the CAVIAR distribution.
+//
+//========================================================================
+
+#ifndef CAVIAR_OBJECTS_FORCEFIELD_UMBRELLA_SAMPLING_H
+#define CAVIAR_OBJECTS_FORCEFIELD_UMBRELLA_SAMPLING_H
+
+#include "caviar/objects/force_field.h"
+
+CAVIAR_NAMESPACE_OPEN
+
+namespace force_field {
+
+/**
+ * This class does a spring force-field on the molecular bonds
+ *  
+ */
+class Umbrella_sampling : public Force_field {
+public:
+  Umbrella_sampling (class CAVIAR *);
+  ~Umbrella_sampling () {};
+
+  //double energy();
+
+  bool read (class caviar::interpreter::Parser *);
+  void verify_settings ();
+  void calculate_acceleration ();
+public:
+  /**
+   * If true, the data is written in the file
+  */
+  bool data_production = false;
+
+  /**
+   * The elastic coefficient of the force
+  */
+  double elastic_coef = 0;
+
+  /**
+   * The atom_id in which the force is acted on
+  */
+  int atom_id = -1;
+
+  /**
+   * The anchor point position, i.e., the constraint position
+  */
+  caviar::Vector<double> position {0,0,0};
+
+  /**
+   * The spring 
+  */
+  caviar::Vector<double> dr;
+
+  /**
+   * Count number of files which is produced
+  */
+  int file_counter = 0;
+
+  /**
+   * Count the number of steps locally in order to use in skipping frames
+  */
+  int step_counter = 0;
+
+  /**
+   * Every number of 'step' the data is written in the file
+  */
+  int step = 1;
+
+  std::ofstream ofs_data;
+
+  std::string file_name_data = "o_umbrella_";
+};
+
+} //force_field
+
+CAVIAR_NAMESPACE_CLOSE
+
+#endif
---
+++ src/caviar/objects/force_field/umbrella_sampling.cpp
@@ -0,0 +1,120 @@
+
+//========================================================================
+//
+// Copyright (C) 2019 by Morad Biagooi and Ehsan Nedaaee Oskoee.
+//
+// This file is part of the CAVIAR package.
+//
+// The CAVIAR package is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the CAVIAR distribution.
+//
+//========================================================================
+
+#include "caviar/objects/force_field/umbrella_sampling.h"
+#include "caviar/utility/interpreter_io_headers.h"
+#include "caviar/objects/neighborlist.h"
+#include "caviar/objects/atom_data.h"
+#include "caviar/objects/domain.h"
+
+#include <cmath>
+#include <iomanip>
+
+CAVIAR_NAMESPACE_OPEN
+
+namespace force_field
+{
+
+  Umbrella_sampling::Umbrella_sampling(CAVIAR *fptr) : Force_field{fptr}
+  {
+    FC_OBJECT_INITIALIZE_INFO
+  }
+
+  bool Umbrella_sampling::read(caviar::interpreter::Parser *parser)
+  {
+    FC_OBJECT_READ_INFO
+    bool in_file = true;
+
+    while (true)
+    {
+      GET_A_TOKEN_FOR_CREATION
+      auto t = token.string_value;
+      if (string_cmp(t, "elastic_coef"))
+      {
+        GET_OR_CHOOSE_A_REAL(elastic_coef, "", "")
+        if (elastic_coef < 0)
+          error->all(FC_FILE_LINE_FUNC_PARSE, "Elastic coef. have to be non-negative.");
+      }
+      else if (string_cmp(t, "atom_id"))
+      {
+        GET_OR_CHOOSE_A_INT(atom_id, "", "")
+        if (atom_id < 0)
+          error->all(FC_FILE_LINE_FUNC_PARSE, "atom_id have to be non-negative.");
+      }
+      else if (string_cmp(t, "set_position"))
+      {
+        double x,y,z;
+        GET_OR_CHOOSE_A_REAL(x, "", "")
+        GET_OR_CHOOSE_A_REAL(y, "", "")
+        GET_OR_CHOOSE_A_REAL(z, "", "")
+        position = Vector<Real_t>{x, y, z};
+      }
+        else if (string_cmp(t, "set_position_x"))
+      {
+        GET_OR_CHOOSE_A_REAL(position.x, "", "")
+      }
+        else if (string_cmp(t, "set_position_y"))
+      {
+        GET_OR_CHOOSE_A_REAL(position.y, "", "")
+      }
+        else if (string_cmp(t, "set_position_z"))
+      {
+        GET_OR_CHOOSE_A_REAL(position.z, "", "")
+      }
+      else if (string_cmp(t, "set_atom_data") || string_cmp(t, "atom_data"))
+      {
+        FIND_OBJECT_BY_NAME(atom_data, it)
+        atom_data = object_container->atom_data[it->second.index];
+      }
+      else
+        FC_ERR_UNDEFINED_VAR(t)
+    }
+
+    return in_file;
+  }
+
+  void Umbrella_sampling::verify_settings()
+  {
+    FC_NULLPTR_CHECK(atom_data)
+    //my_mpi_rank = atom_data->get_mpi_rank();
+  }
+
+  void Umbrella_sampling::calculate_acceleration()
+  {
+    FC_OBJECT_VERIFY_SETTINGS
+
+    auto &type = atom_data->atom_struct_owned.type;
+    auto &mass_inv = atom_data->atom_type_params.mass_inv;
+    int i = atom_data->atom_id_to_index[atom_id];
+    auto pos_i = atom_data->atom_struct_owned.position[i];
+    
+#if defined(CAVIAR_WITH_MPI)
+    dr = position - pos_i;
+#else
+    dr = domain->periodic_distance(position - pos_i);
+#endif
+
+    //const auto dr_sq = dr * dr;
+    //const auto dr_norm = std::sqrt(dr_sq);
+    //const auto dr_vec = dr / dr_norm;
+    const auto force = -elastic_coef *dr ;
+    atom_data->atom_struct_owned.acceleration[i] -= force * mass_inv[type[i]];
+
+  }
+
+} // force_field
+
+CAVIAR_NAMESPACE_CLOSE
